7dd96ec1fc767e220284884bd1189ebdfdebed04
Krisziah <155798339+KRISZIAH@users.noreply.github.com>
2024-03-03 09:22:42 +0800

    Add files via upload

---
+++ Assets/script.js
@@ -0,0 +1,15 @@
+// Code:
+
+    document.addEventListener("DOMContentLoaded", function() {
+        var navbarToggler = document.querySelector(".navbar-toggler");
+        var navbarCollapse = document.querySelector("#navbarSupportedContent");
+
+        navbarToggler.addEventListener("click", function() {
+            if (navbarCollapse.classList.contains("show")) {
+                navbarCollapse.classList.remove("show");
+            } else {
+                navbarCollapse.classList.add("show");
+            }
+        });
+    });
+
